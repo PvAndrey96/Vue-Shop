d432489861c5f9440ec6bf2705c91543eae3c643
PvAndrey92 <pv.andrey.96@gmail.com>
2020-08-14 13:18:27 +0300

    Commit

---
+++ src/components/base/VButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <button class="flex items-center justify-center h-10 px-5 text-white uppercase transition-colors duration-200 bg-theme hover:bg-theme-accent">
+    <slot/>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'VButton',
+};
+</script>
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -2,8 +2,8 @@
   <VContainer class="py-6 lg:py-4">
     <VBreadcrumbs class="mb-8"/>
     <div class="grid grid-cols-3 gap-8">
-      <div class="col-span-2">
-        <div class="grid grid-flow-col grid-cols-3 grid-rows-2 gap-4">
+      <div class="col-span-2 lg:col-span-3">
+        <div class="grid grid-flow-col grid-cols-3 grid-rows-2 gap-4 sm:gap-2">
           <img
             v-for="(img, id) in images"
             :key="id"
@@ -12,7 +12,7 @@
           />
         </div>
       </div>
-      <div class="col-span-1">
+      <div class="col-span-1 lg:col-span-3">
         <h1 class="mb-1 font-bold uppercase text-6 text-black-70">Футболка мужская</h1>
         <div class="font-medium uppercase mb-7 text-5 text-black-70">1180 rub</div>
         <div class="mb-7">
@@ -49,6 +49,13 @@
             </li>
           </ul>
         </div>
+        <div class="mb-6">
+          <div class="mb-2 font-bold uppercase text-theme">Описание</div>
+          <div class="font-medium">
+            Шорты Focus - это продолжение минималистичной линии бренда, с мощными улучшениями в сторону деталей и стиля. Новый дизайн шорт, пошив и карманы на молнии - идеально подойдут как во время тяжелых активных тренировок, так и для повседневного уличного стиля или деловой встречи. Посадка шорт и конструкция швов, а также эластичная ткань позволяет не сковывать движения, давая максимальную свободу для любой активности. Вставка Road to the Dream создана, чтобы дополнять дизайн и вдохновлять на фокус над мечтой и путь ее достижения!
+          </div>
+        </div>
+        <VButton>Добавить в корзину</VButton>
       </div>
     </div>
   </VContainer>
@@ -58,12 +65,13 @@
 import VBreadcrumbs from '@/components/base/VBreadcrumbs.vue';
 import VButtonColor from '@/components/base/VButtonColor.vue';
 import VButtonSize from '@/components/base/VButtonSize.vue';
+import VButton from '@/components/base/VButton.vue';
 // import { mapGetters, mapMutations } from 'vuex';
 
 export default {
   name: 'Product',
   components: {
-    VBreadcrumbs, VButtonColor, VButtonSize,
+    VBreadcrumbs, VButtonColor, VButtonSize, VButton,
   },
   data() {
     return {
