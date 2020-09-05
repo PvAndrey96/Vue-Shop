6f10b863840d9daa2ed6e8db769970f10986273d
PvAndrey92 <pv.andrey.96@gmail.com>
2020-09-05 10:40:49 +0300

    Commit

---
+++ src/components/base/VModalWindow.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="fixed top-0 bottom-0 left-0 right-0 z-10 flex items-center justify-center">
+    <div
+      class="absolute w-full h-full bg-black-50"
+      @click="$emit('close')"
+    >
+    </div>
+    <div class="relative w-full max-w-sm p-4 bg-white">
+      <button
+        class="absolute right-0 px-4"
+        @click="$emit('close')"
+      >
+        <VSvg
+          class="w-6 h-6 text-black-30"
+          name="close"
+        />
+      </button>
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VModalWindow',
+};
+</script>
--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,23 +1,42 @@
 <template>
-  <VContainer class="py-6 lg:py-4">
-    <VBreadcrumbs class="mb-8"/>
-    <h1 class="mb-6 font-medium text-8">Оформление заказа</h1>
-    <div class="grid grid-cols-6 gap-8 lg:gap-0">
-      <TheCheckoutForm/>
-      <TheCheckoutInfo/>
-    </div>
-  </VContainer>
+  <div>
+    <VContainer class="py-6 lg:py-4">
+      <VBreadcrumbs class="mb-8"/>
+      <h1 class="mb-6 font-medium text-8">Оформление заказа</h1>
+      <div class="grid grid-cols-6 gap-8 lg:gap-0">
+        <TheCheckoutForm/>
+        <TheCheckoutInfo/>
+      </div>
+    </VContainer>
+    <VModalWindow
+      v-if="false"
+    >
+      <div class="p-8 text-center">
+        <div class="font-bold uppercase text-4">Ваш заказ успешно оформлен</div>
+        <div class="mt-2 text-4">Наш менеджер свяжется с вами в ближайшее время для подтверждения заказа и уточнения деталей доставки и оплаты.
+        Спасибо за заказ!</div>
+      </div>
+      <VButton
+        full
+        small
+      >
+        Закрыть
+      </VButton>
+    </VModalWindow>
+  </div>
 </template>
 
 <script>
 import TheCheckoutForm from '@/components/TheCheckoutForm.vue';
 import TheCheckoutInfo from '@/components/TheCheckoutInfo.vue';
 import VBreadcrumbs from '@/components/base/VBreadcrumbs.vue';
+import VModalWindow from '@/components/base/VModalWindow.vue';
+import VButton from '@/components/base/VButton.vue';
 
 export default {
   name: 'Checkout',
   components: {
-    VBreadcrumbs, TheCheckoutInfo, TheCheckoutForm,
+    VBreadcrumbs, TheCheckoutInfo, TheCheckoutForm, VModalWindow, VButton,
   },
 };
 </script>
