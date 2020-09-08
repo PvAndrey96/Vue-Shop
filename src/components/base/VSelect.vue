34821c572cb3e3947e43eba1f70ed3da739cbd4a
PvAndrey92 <pv.andrey.96@gmail.com>
2020-09-08 09:45:51 +0300

    Commit

--- src/components/base/VSelect.vue
+++ src/components/base/VSelect.vue
@@ -1,18 +1,71 @@
 <template>
-  <div class="relative flex w-56 h-10 border">
-    <select class="w-full pl-4 pr-10 bg-transparent text-5">
-      <option class="block h-10">Сначала дороже</option>
-      <option>Сначала дешевле</option>
-    </select>
-    <VSvg
-      name='arrow-down'
-      class="absolute right-0 self-center mx-2 h-7 w-7 text-black-10"
-    />
+  <div class="relative w-56 font-medium text-5 text-black-70">
+    <div
+      class="flex h-10 px-4 border cursor-pointer"
+      @click='optionsOpen = !optionsOpen'
+    >
+      <div
+        class="flex items-center flex-grow"
+        :class="{'text-black-30': !selected}"
+      >
+        {{text}}
+      </div>
+      <VSvg
+        name='arrow-down'
+        class="w-6 -mr-2 text-black-10"
+      />
+    </div>
+    <div
+      v-if="optionsOpen"
+      class="absolute w-full py-1 mt-2 bg-white border"
+    >
+      <div
+        v-for="option in options"
+        :key="option.value"
+        class="flex items-center h-10 px-4"
+        @click="selectOption(option)"
+      >
+        {{option.text}}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'VSelect',
+  model: {
+    prop: 'selected',
+    event: 'select',
+  },
+  props: {
+    options: {
+      type: Array,
+      required: true,
+    },
+    selected: {
+      type: Object,
+    },
+    placeholder: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      optionsOpen: false,
+    };
+  },
+  computed: {
+    text() {
+      return this.selected ? this.selected.text : this.placeholder;
+    },
+  },
+  methods: {
+    selectOption(option) {
+      this.$emit('select', option);
+      this.optionsOpen = false;
+    },
+  },
 };
 </script>
